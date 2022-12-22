8ef7eb65c311127f64dc63d25d248bad60b3d4d0
Achmad Muzadi <achmadmuzadi05@gmail.com>
2022-12-23 00:02:02 +0700

    count task

--- controllers/taskController.go
+++ controllers/taskController.go
@@ -15,21 +15,30 @@ type Task struct {
 	StartTask string `json:"start_task" form:"StartTask" binding:"required"`
 	EndTask   string `json:"end_task" form:"EndTask" binding:"required"`
 	Category  string `json:"category" form:"Category" binding:"required"`
+	Status    string `json:"status" form:"Status" binding:"required"`
+	UserID    int    `json:"user_id" form:"UserID" binding:"required"`
 }
 
 func (h handler) GetTasks(c *gin.Context) {
 
 	var tasks []model.Task
 
+	allCount := h.DB.Find(&tasks).RowsAffected
+	countSuccess := h.DB.Where("status = ?", "1").Find(&tasks).RowsAffected
+	countError := h.DB.Where("status = ?", "0").Find(&tasks).RowsAffected
 	if result := h.DB.Find(&tasks); result.Error == nil {
 		c.JSON(http.StatusOK, gin.H{
-			"status": http.StatusOK,
-			"data":   &tasks,
+			"status":        http.StatusOK,
+			"data":          &tasks,
+			"all_count":     allCount,
+			"count_success": countSuccess,
+			"count_errror":  countError,
+			"success":       "Success",
 		})
 	} else {
 		c.JSON(http.StatusNotFound, gin.H{
-			"status": http.StatusNotFound,
-			"data":   "Data tidak ada",
+			"status":  http.StatusNotFound,
+			"success": "Data tidak ada",
 		})
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
@@ -41,13 +50,14 @@ func (h handler) GetTask(c *gin.Context) {
 	var task model.Task
 	if result := h.DB.First(&task, id); result.Error == nil {
 		c.JSON(http.StatusOK, gin.H{
-			"status": http.StatusOK,
-			"data":   &task,
+			"status":  http.StatusOK,
+			"data":    &task,
+			"success": "success",
 		})
 	} else {
 		c.JSON(http.StatusNotFound, gin.H{
-			"status": http.StatusNotFound,
-			"data":   "Data tidak ada",
+			"status":  http.StatusNotFound,
+			"success": "Data tidak ada",
 		})
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
@@ -71,17 +81,20 @@ func (h handler) StoreTask(c *gin.Context) {
 	task.StartTask = req.StartTask
 	task.EndTask = req.EndTask
 	task.Category = req.Category
+	task.Status = req.Status
+	task.UserID = req.UserID
 
 	if result := h.DB.Create(&task); result.Error == nil {
 		c.JSON(http.StatusOK, gin.H{
-			"status": http.StatusOK,
-			"data":   "Data berhasil ditambahkan",
-			"result": &task,
+			"status":  http.StatusOK,
+			"success": "Success",
+			"data":    "Data berhasil ditambahkan",
+			"result":  &task,
 		})
 	} else {
 		c.JSON(http.StatusNotFound, gin.H{
-			"status": http.StatusNotFound,
-			"data":   "Gagal menambahkan data",
+			"status":  http.StatusNotFound,
+			"success": "Gagal menambahkan data",
 		})
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
@@ -96,7 +109,7 @@ func (h handler) UpdateTask(c *gin.Context) {
 	if result := h.DB.First(&task, id); result.Error != nil {
 		c.JSON(http.StatusNotFound, gin.H{
 			"status": http.StatusNotFound,
-			"data":   "Data tidak ada",
+			"sucess": "Data tidak ada",
 		})
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
@@ -114,16 +127,19 @@ func (h handler) UpdateTask(c *gin.Context) {
 	task.StartTask = req.StartTask
 	task.EndTask = req.EndTask
 	task.Category = req.Category
+	task.Status = req.Status
+	task.UserID = req.UserID
 	if result := h.DB.Save(&task); result.Error == nil {
 		c.JSON(http.StatusOK, gin.H{
-			"status": http.StatusOK,
-			"data":   "Berhasil mengupdate data",
-			"result": &task,
+			"status":  http.StatusOK,
+			"data":    "Berhasil mengupdate data",
+			"success": "Success",
+			"result":  &task,
 		})
 	} else {
 		c.JSON(http.StatusNotFound, gin.H{
-			"status": http.StatusNotFound,
-			"data":   "Gagal mengupdate data",
+			"status":  http.StatusNotFound,
+			"success": "Gagal mengupdate data",
 		})
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
@@ -136,8 +152,8 @@ func (h handler) DeleteTask(c *gin.Context) {
 
 	if result := h.DB.First(&task, id); result.Error != nil {
 		c.JSON(http.StatusNotFound, gin.H{
-			"status": http.StatusNotFound,
-			"data":   "Data tidak ada",
+			"status":  http.StatusNotFound,
+			"success": "Data tidak ada",
 		})
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
@@ -145,14 +161,15 @@ func (h handler) DeleteTask(c *gin.Context) {
 
 	if result := h.DB.Delete(&task); result.Error == nil {
 		c.JSON(http.StatusOK, gin.H{
-			"status": http.StatusOK,
-			"data":   "Data berhasil dihapus",
-			"result": &task,
+			"status":  http.StatusOK,
+			"data":    "Data berhasil dihapus",
+			"success": "Success",
+			"result":  &task,
 		})
 	} else {
 		c.JSON(http.StatusNotFound, gin.H{
-			"status": http.StatusNotFound,
-			"data":   "Gagal menghapus data",
+			"status":  http.StatusNotFound,
+			"success": "Gagal menghapus data",
 		})
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557 // indirect
+	github.com/766b/go-outliner v0.0.0-20180511142203-fc6edecdadd7 // indirect
 	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
 	github.com/codegangsta/gin v0.0.0-20211113050330-71f90109db02 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
